37999790e21aeada5be2a8d4a40c5cdb56cb8410
ltdrobot <ltdrobot@localizethedocs.org>
2026-09-15 14:00:32 +0000

    Deploying to pages from @ localizethedocs/prql-book-l10n@abbb4a68a407e4555c45e3c2fd9ecb3b60409c4b 🚀

---
+++ ltd-config.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",     "English"],
+    ["zh-cn",     "简体中文"],
+    ["zh-tw",     "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["main",      "Development"],
+    ["0",         "Release 0.x"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",     "https://projects.localizethedocs.org"],
+    ["Crowdin",   "https://localizethedocs.crowdin.com/prql-book-l10n"],
+    ["GitHub",    "https://github.com/localizethedocs/prql-book-l10n"],
+    ["AtomGit",   "https://atomgit.com/localizethedocs/prql-book-l10n"],
+    ["GitFlic",   "https://gitflic.ru/project/localizethedocs/prql-book-l10n"],
+  ]
+};
